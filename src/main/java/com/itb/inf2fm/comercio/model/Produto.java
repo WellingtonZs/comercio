a36c2bf28283c823fc378ea298e33dc8f03174d8
Wellington Cavalcante Ferreira de Lima <wellingtonzsescola@gmail.com>
2023-06-06 12:30:19 -0300

    Inicio controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,10 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String cadigobarras;
+	private double preco;
+}
